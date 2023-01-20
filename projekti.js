93c9c42a180a479cf14a2ffeb57ec7a227996bc2
arianit-gashi <98714216+arianit-gashi@users.noreply.github.com>
2023-01-19 16:23:04 -0800

    Add files via upload

--- projekti.js
+++ projekti.js
@@ -5,6 +5,7 @@
 
 $(document).ready(function(){
 
+    //selektoret---------------------------------------------------
     $(".sidebar").css('background-color', '#df2d45');
 
     $(".mt-3").css('color', 'white');
@@ -23,6 +24,48 @@ $(document).ready(function(){
     $(".title").css('font-size', '35px');
     $(".title").css('font-weight', '60');
 
+    //eventet----------------------------------------------------------
+    $("#one").mouseenter(function()
+    {
+      $("#one").css('width', '120px')
+    })
+    $("#one").mouseleave(function()
+    {
+      $("#one").css('width', '100px')
+    })
+    $("#two").mouseenter(function()
+    {
+      $("#two").css('width', '120px')
+    })
+    $("#two").mouseleave(function()
+    {
+      $("#two").css('width', '100px')
+    })
+    $("#three").mouseenter(function()
+    {
+      $("#three").css('width', '120px')
+    })
+    $("#three").mouseleave(function()
+    {
+      $("#three").css('width', '100px')
+    })
+    $("#four").mouseenter(function()
+    {
+      $("#four").css('width', '120px')
+    })
+    $("#four").mouseleave(function()
+    {
+      $("#four").css('width', '100px')
+    })
+    $("#five").mouseenter(function()
+    {
+      $("#five").css('width', '120px')
+    })
+    $("#five").mouseleave(function()
+    {
+      $("#five").css('width', '100px')
+    })
+
     $(".content").mouseenter(function()
     {
         
@@ -35,21 +78,70 @@ $(document).ready(function(){
         $(".content").css('color', '#4f4f4f');
     })
 
+    
+    //efektet-----------------------------------------
+    //hide
+    $("#btn1" ).click(function()
+    {
+       $("#body1").hide();
+    })
+    //show
+    $("#btn2" ).click(function()
+    {
+       $("#body1").show(200);
+    })
 
+    //fade
 
-
-
-
-
-
-
+    $(".butoni1").click(function()
+    {
+      $("#njeshi").fadeIn(1500);
+    })
+    $("#largo1").click(function()
+    {
+      $("#njeshi").fadeOut(1500);
+    })
+    $(".butoni1").click(function()
+    {
+      $("#secondd").fadeIn(1500);
+    })
+    $("#largo2").click(function()
+    {
+      $("#secondd").fadeOut(1500);
+    })
+    $(".butoni1").click(function()
+    {
+      $("#third").fadeIn(1500);
+    })
+    $("#largo3").click(function()
+    {
+      $("#third").fadeOut(1500);
+    })
+    //slide
+    
+    $("#buttton").click(function()
+    {
+      $("#contenti1").slideUp(2000);
+    })
+    $("#butttoni").click(function()
+    {
+      $("#contenti1").slideDown(2000);
+    })
+   
+    //animate
     $(".img").hover(function(){
 
-      $(this).animate({width: "280px", height: "280px", left: "10px"}, "slow");
+      $(this).animate({
+                    width: "280px", 
+                    height: "280px", 
+                    left: "10px"}, "slow");
 
     },
      function(){
 
-      $(this).animate({width: "250px", height: "250px", left: "10px"}, "slow");
+      $(this).animate({
+                    width: "250px", 
+                    height: "250px", 
+                    left: "10px"}, "slow");
     });
 });
